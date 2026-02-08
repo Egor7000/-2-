273cf91695ffdc8d9bc4759ab9da325f24bfeb2e
Egor7000 <ebusurin@bk.ru>
2026-02-08 23:35:41 +0300

    ЛР2 Атрибуты и методы

---
+++ Задание1.py
@@ -0,0 +1,45 @@
+BOOKS_DATABASE = [
+    {
+        "id": 1,
+        "name": "test_name_1",
+        "pages": 200,
+    },
+    {
+        "id": 2,
+        "name": "test_name_2",
+        "pages": 400,
+    }
+]
+
+
+class Book:
+    def __init__(self, id_: int, name: str, pages: int) -> None:
+        if id_ <= 0:
+            raise ValueError("id книги должен быть положительным числом")
+        if not name:
+            raise ValueError("Название книги не может быть пустым")
+        if pages <= 0:
+            raise ValueError("Количество страниц должно быть положительным")
+
+        self.id = id_
+        self.name = name
+        self.pages = pages
+
+    def __str__(self) -> str:
+        return f'Книга "{self.name}"'
+
+    def __repr__(self) -> str:
+        return f"Book(id_={self.id}, name='{self.name}', pages={self.pages})"
+
+
+if __name__ == '__main__':
+    # инициализируем список книг
+    list_books = [
+        Book(id_=book_dict["id"], name=book_dict["name"], pages=book_dict["pages"])
+        for book_dict in BOOKS_DATABASE
+    ]
+
+    for book in list_books:
+        print(book)  # проверяем метод __str__
+
+    print(list_books)  # проверяем метод __repr__
